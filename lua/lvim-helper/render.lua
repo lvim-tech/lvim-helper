818dba17257601970bd5e23ffa5471e2e6b40ff4
Biser Stoilov <lvim@motion-agency.com>
2021-06-20 19:25:32 +0300

    Add hl for title

--- lua/lvim-helper/render.lua
+++ lua/lvim-helper/render.lua
@@ -4,17 +4,24 @@ local settings = require 'lvim-helper.settings'
 
 local M = {}
 
+local namespace_id = api.nvim_create_namespace('LvimHelperHighlights')
+
 function M.render(file, num, all)
     local lines = utils.readlines(file)
     for k, v in pairs(lines) do
         lines[k] = '  ' .. lines[k] --
     end
     vim.api.nvim_buf_set_option(settings.settings.buf, 'modifiable', true)
-    api.nvim_buf_set_lines(buf, 0, -1, false, {
-        '  ***** Helper ' .. num .. ' / ' .. all .. ' *****  '
+
+    api.nvim_buf_set_lines(buf, 0, -1, true, {
+        '  ----- Helper ' .. num .. ' / ' .. all .. ' -----  '
     })
     api.nvim_buf_set_lines(buf, 1, -1, false, {' '})
     api.nvim_buf_set_lines(settings.settings.buf, 2, -1, false, lines)
+
+    api.nvim_buf_add_highlight(settings.settings.buf, namespace_id,
+                               'LvimHelperTitle', 0, 0, -1)
+
     vim.api.nvim_buf_set_option(settings.settings.buf, 'modifiable', false)
 end
 
